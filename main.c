d3191c6703c15dae4a7da63bc9f9105d91213688
siru02 <khm32323@gmail.com>
2024-11-30 21:58:19 +0900

    [feat]: add 8x8

--- main.c
+++ main.c
@@ -18,6 +18,7 @@ int main()
     printf("enter 'input file name': ");
     scanf("%s", filename);
     split_matrix_4(filename);
+    split_matrix_8(filename);
     return 0;
 }
 
@@ -108,7 +109,7 @@ void split_matrix_8(const char* fileName)
     FILE *files[NUM_CLIENTS];
     for (int i = 0; i < NUM_CLIENTS; ++i) {
         char filename[256];
-        snprintf(filename, sizeof(filename), "block_%d.txt", i);
+        snprintf(filename, sizeof(filename), "8x8_%d.txt", i);
         files[i] = fopen(filename, "w");
         if (files[i] == NULL) {
             perror("fopen");
@@ -119,11 +120,11 @@ void split_matrix_8(const char* fileName)
         }
     }
     
-    int block_size = MATRIX_SIZE / 4;
+    int block_size = MATRIX_SIZE / 8;
     // 4x4 블록으로 나누어 각 클라이언트(파일)에 저장
-    for (int block_row = 0; block_row < 4; ++block_row) {
-        for (int block_col = 0; block_col < 4; ++block_col) {
-            int file_index = (block_row + block_col) % NUM_CLIENTS;
+    for (int block_row = 0; block_row < 8; ++block_row) {
+        for (int block_col = 0; block_col < 8; ++block_col) {
+            int file_index = (block_row * 8 + block_col) % NUM_CLIENTS;
             
             for (int i = 0; i < block_size; ++i) {
                 for (int j = 0; j < block_size; ++j) {
