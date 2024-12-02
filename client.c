33dc39f0e3e148faea246ef689b9a7078bb263fb
siru02 <khm32323@gmail.com>
2024-12-02 16:53:13 +0900

    [feat]: 8개프로세스생성 간소화

--- client.c
+++ client.c
@@ -3,8 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
-#include <sys/socket.h>
-#include <dirent.h>
+#include <sys/wait.h>
 
 void checkForkError(pid_t pid) {
     if (pid < 0) {
@@ -13,78 +12,33 @@ void checkForkError(pid_t pid) {
     }
 }
 
-// 부모프로세스는 자식프로세스가 종료될 때까지 wait할것
-int main()
-{
+int main() {
+    pid_t pids[8]; // 8개의 프로세스 ID를 저장
     int client_num = 0;
-    pid_t pid = getpid();
-    pid_t cPid = fork(); //cPid는 fork시마다 계속 덮어쓰는 변수
-    checkForkError(cPid);
-    pid_t sm3 = cPid; // child->self->self
 
-    if (cPid == 0) { // child
-        cPid = fork();
+    // for문으로 fork를 7번 수행하면
+    for (int i = 0; i < 7; i++) {
+        pid_t cPid = fork();
         checkForkError(cPid);
-        pid_t sm1 = cPid; //child->child->self
 
-        if (cPid == 0) { // child -> child
-            cPid = fork(); //child->child->child sm0
-            checkForkError(cPid);
-
-            if (cPid == 0) { // child->child->child //sm0
-                
-            }
-            else { // child->child->self //sm1
-
-                waitpid(cPid, NULL, 0); //직속 자식인 sm0의 종료를 기다린다
-            }
-        }
-        else { //child->self
-            cPid = fork();
-            checkForkError(cPid);
-
-            if (cPid == 0) { //child->self->child sm2
-
-                waitpid(sm1, NULL, 0); //sm1의 종료를 기다린다
-            }
-            else { //child->self->self sm3
-
-                waitpid(cPid, NULL, 0); //직속 자식인 sm2의 종료를 기다린다
-            }
+        if (cPid == 0) {
+            // 자식 프로세스
+            client_num = i; // 각 클라이언트 번호
+            printf("Client Process %d (PID: %d, Parent PID: %d) started.\n", client_num, getpid(), getppid());
+            sleep(1); // 클라이언트 작업 시뮬레이션
+            printf("Client Process %d (PID: %d) exiting.\n", client_num, getpid());
+            exit(0); // 작업 종료
+        } else {
+            // 부모 프로세스
+            pids[i] = cPid; // 자식 PID 저장
         }
     }
-    else { //self
-        pid_t sm3 = cPid; // child->self->self;
-        cPid = fork();
-        checkForkError(cPid);
-        pid_t sm6 = cPid; //self->child->self sm5
 
-        if (cPid == 0) { // self -> child
-            cPid = fork();
-            checkForkError(cPid);
-
-            if (cPid == 0) { //self->child->child sm4
-
-                waitpid(sm3, NULL, 0); //wait child->self->self sm3
-            }
-            else { //self->child->self sm5
-
-                waitpid(cPid, NULL, 0); // wait self->child->child sm4
-            }
-        }
-        else { // self->self
-            pid_t sm5 = cPid; //self->child->self
-            cPid = fork();
-            checkForkError(cPid);
-
-            if (cPid == 0) { //self->self->child sm6
-
-                waitpid(sm5, NULL, 0); //wait self->child->self sm5
-            }
-            else { //self->self->self sm7
-
-                waitpid(cPid, NULL, 0);
-            }
-        }
+    // 부모는 모든 자식의 종료를 기다림
+    for (int i = 0; i < 7; i++) { //부모프로세스인 sm7은 대기할 필요가 없음
+        waitpid(pids[i], NULL, 0); // 자식 프로세스 종료 대기
     }
+
+    printf("All child processes exited. Parent Process (PID: %d) exiting.\n", getpid());
+    return 0;
 }
